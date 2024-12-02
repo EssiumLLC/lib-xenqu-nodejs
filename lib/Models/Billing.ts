e14bb63203d7f996ee0a3fa37a7f6522edba8b93
Levi Asiala <levi.asiala@essiumlabs.com>
2024-12-02 14:09:08 -0500

    Updated lib to match latest api changes

--- lib/Models/Billing.ts
+++ lib/Models/Billing.ts
@@ -11,7 +11,27 @@ const BillingCustomerSchema = z.object({
     })
 });
 
+const BillingActivityScehma = z.array(
+  z.object({
+    process_date: z.date(),
+    new_records_count: z.number(),
+    engaged_records_count: z.number(),
+    active_records_count: z.number(),
+  })
+);
+
+const BillingOverviewScehma = z.array(
+  z.object({
+    cycle_date: z.date(),
+    new_records_count: z.number(),
+    engaged_records_count: z.number(),
+    active_records_count: z.number(),
+  })
+);
+
 type BillingSetupSecret = z.infer<typeof BillingSetupSecretSchema>;
 type BillingCustomer = z.infer<typeof BillingCustomerSchema>;
+type BillingActivity = z.infer<typeof BillingActivityScehma>;
+type BillingOverview = z.infer<typeof BillingOverviewScehma>;
 
-export { BillingSetupSecret, BillingSetupSecretSchema, BillingCustomer, BillingCustomerSchema };
+export { BillingSetupSecret, BillingSetupSecretSchema, BillingCustomer, BillingCustomerSchema, BillingActivity, BillingActivityScehma, BillingOverview, BillingOverviewScehma };
--- lib/Routes/BillingRoutes.ts
+++ lib/Routes/BillingRoutes.ts
@@ -1,5 +1,5 @@
 import XenquAPI from "..";
-import { BillingCustomer, BillingSetupSecret } from "../Models/Billing";
+import { BillingActivity, BillingCustomer, BillingSetupSecret } from "../Models/Billing";
 
 export default class BillingRoutes {
 
@@ -17,11 +17,23 @@ export default class BillingRoutes {
         return XenquAPI.Base.makeGet(`/billing/payment-method/list`)
     }
 
+    public GetBillingActivity(days_away: string): Promise<BillingActivity> {
+        return XenquAPI.Base.makeGet(`/billing/meter-event/summary`,{days_away: days_away});
+    }
+
+    public GetBillingOverview(): Promise<any> {
+        return XenquAPI.Base.makeGet(`/billing/meter-event/overview`);
+    }
+
     public DetachPaymentMethod(payment_method: string): Promise<any> {
         return XenquAPI.Base.makeDelete(`/billing/payment-method/` + payment_method);
     }
 
     public SetDefaultPaymentMethod(payment_method: string): Promise<any> {
         return XenquAPI.Base.makePut(`/billing/payment-method/` + payment_method + `/default`,null);
+    }
+
+    public AcceptTerms(terms: object): Promise<any> {
+        return XenquAPI.Base.makePost(`/billing/terms/accept`,JSON.stringify(terms));
     }
 }
